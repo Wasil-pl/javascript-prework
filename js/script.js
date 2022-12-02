0353e744647e7b1328f518eba41c126b93c600b4
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2022-12-02 15:55:30 +0100

    Add player moves

--- js/script.js
+++ js/script.js
@@ -27,3 +27,23 @@ if(randomNumber == 3){
 }
 
 printMessage('Ruch komputera to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+if(playerInput == '2'){
+	playerMove = 'papier';
+}
+
+if(playerMove == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
