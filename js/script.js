a7b02fd110f3d434d27725509f4b12ffc3cfcf92
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2022-12-02 15:51:44 +0100

    Add reading computer moves

--- js/script.js
+++ js/script.js
@@ -8,8 +8,22 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = 'kamień';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = 'papier';
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+	computerMove = 'papier';
+}
+
+if(randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+printMessage('Ruch komputera to: ' + computerMove);
